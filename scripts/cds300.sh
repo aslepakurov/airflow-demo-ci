b81c8ec2e7bc38b2b4db64b9bd1f9e15ab6ab91c
aslepakurov <a.slepakurov@gmail.com>
2019-03-06 22:09:15 +0200

    Test real-script

--- scripts/cds300.sh
+++ scripts/cds300.sh
@@ -10,5 +10,4 @@ mkdir -p /tmp/$BAK_DIR
 mkdir -p /tmp/$S3_INBOX
 
 mv /tmp/$DIR/* /tmp/$BAK_DIR
-sshpass -v -p $SSH_PASS scp -P 22 $SSH_USER@sftp-ci.verygoodsecurity.io:/www/ /tmp/1T/00001T_B2KCDS300/B2KCDS300
-#sshpass -p 'password' scp "demo@test.rebex.net:$DIR/*" /tmp/$DIR/
+#sshpass -v -p pass scp -P 22 foo@sftp:/upload/ /tmp/1T/00001T_B2KCDS300/B2KCDS300
